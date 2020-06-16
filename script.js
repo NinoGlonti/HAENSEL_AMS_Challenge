1884e5b3a307ab895208c607addba7d2ac8c5504
NinoGlonti <54588901+NinoGlonti@users.noreply.github.com>
2020-06-16 21:49:35 +0200

    histogram

---
+++ script.js
@@ -0,0 +1,62 @@
+// set the dimensions and margins of the graph
+var margin = { top: 10, right: 30, bottom: 30, left: 40 },
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+var svg = d3
+  .select("#myData")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g") // Group of SVG-s
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//Getting a Data
+d3.csv(
+  "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv",
+  function (data) {
+    // xScale
+    var x = d3.scaleLinear().domain([0, 1000]).range([0, width]);
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    //histogram
+    var histogram = d3
+      .histogram()
+      .value(function (d) {
+        return d.price;
+      })
+      .domain(x.domain())
+      .thresholds(x.ticks(70));
+
+    var bins = histogram(data);
+    //yScale
+    var y = d3.scaleLinear().range([height, 0]);
+    y.domain([
+      0,
+      d3.max(bins, function (d) {
+        return d.length;
+      }),
+    ]);
+    svg.append("g").call(d3.axisLeft(y));
+
+    svg
+      .selectAll("rect")
+      .data(bins)
+      .enter()
+      .append("rect")
+      .attr("x", 1)
+      .attr("transform", function (d) {
+        return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+      })
+      .attr("width", function (d) {
+        return x(d.x1) - x(d.x0) - 1;
+      })
+      .attr("height", function (d) {
+        return height - y(d.length);
+      })
+      .style("fill", "#69b3a2");
+  }
+);
