455012a069e966b94be13f6ba55c7133de3d77ac
NinoGlonti <54588901+NinoGlonti@users.noreply.github.com>
2020-06-18 14:06:17 +0200

    bootstrapNavbar

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 30, bottom: 30, left: 40 },
-  width = 460 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+var margin = { top: 70, right: 50, bottom: 50, left: 50 };
+// width = 460 - margin.left - margin.right,
+//height = 400 - margin.top - margin.bottom;
+
+var width =
+  document.getElementById("myData").clientWidth - margin.right - margin.left;
+var height = width / 1.236 + margin.top + margin.bottom;
 
 var svg = d3
   .select("#myData")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
+  .attr("class", "graph-svg-component")
+
   .append("g") // Group of SVG-s
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
